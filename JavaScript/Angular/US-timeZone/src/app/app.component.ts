5efa9cb8317bd5dd5ff3dcc812e578ecdee8433a
Andrew Chang <achang232@comcast.net>
2017-10-09 23:22:31 -0500

    finished date picker wireframe

--- JavaScript/Angular/US-timeZone/src/app/app.component.ts
+++ JavaScript/Angular/US-timeZone/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+   time = new Date();
+   tz= 'Central Standard Time'
+   lastTimeZone = null;
+
+   dateByZone(timezone){
+      this.time = new Date();
+      if (timezone === 'PST'){
+         this.tz='Pacific Standard Time'
+         this.time.setHours(this.time.getHours() - 2)
+      } else if (timezone === 'MST') {
+         this.tz = 'Mountain Standard Time'
+         this.time.setHours(this.time.getHours() - 1)
+      } else if (timezone === 'CST') {
+         this.tz = 'Central Standard Time'
+         this.time.setHours(this.time.getHours())
+      } else if (timezone === 'EST') {
+         this.tz = 'Eastern Standard Time'
+         this.time.setHours(this.time.getHours() + 2)
+      }
+      this.lastTimeZone = timezone
+   }
+
 }
